468a6858760e458c627826340b51281025a675b9
snoopy <thanhyen107@gmail.com>
2021-10-24 15:48:14 +0700

    add WebDriverManager library

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,13 +1,108 @@
 package commons;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
+	protected WebDriver driver;
 	protected final Log log;
+
+	private enum BROWSER {
+		CHROME, FIREFOX, IE, SAFARI, H_CHOME, H_FIREFOX, EDGE_CHROMIUM, EDGE_LEGACY
+	}
 
 	protected BaseTest() {
 		log = LogFactory.getLog(getClass());
+	}
+
+	protected WebDriver getBrowserDriver(String browserName, String appURL) {
+		BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
+		if (browser == BROWSER.CHROME) {
+			// cách 2: sử dụng webDriver manage
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser == BROWSER.FIREFOX) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser == BROWSER.EDGE_CHROMIUM) {
+			WebDriverManager.chromiumdriver().setup();
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please input valid browser name!");
+		}
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(appURL);
+		return driver;
+	}
+
+	protected void closeBrowserAndDriver() {
+		String cmd = "";
+		try {
+			String osName = System.getProperty("os.name").toLowerCase();
+			log.info("OS name = " + osName);
+
+			String driverInstanceName = driver.toString().toLowerCase();
+			log.info("Driver instance name = " + osName);
+
+			if (driverInstanceName.contains("chrome")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq chromedriver*\"";
+				} else {
+					cmd = "pkill chromedriver";
+				}
+			} else if (driverInstanceName.contains("internetexplorer")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq IEDriverServer*\"";
+				}
+			} else if (driverInstanceName.contains("firefox")) {
+				if (osName.contains("windows")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq geckodriver*\"";
+				} else {
+					cmd = "pkill geckodriver";
+				}
+			} else if (driverInstanceName.contains("edge")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq msedgedriver*\"";
+				} else {
+					cmd = "pkill msedgedriver";
+				}
+			} else if (driverInstanceName.contains("opera")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq operadriver*\"";
+				} else {
+					cmd = "pkill operadriver";
+				}
+			} else if (driverInstanceName.contains("safari")) {
+				if (osName.contains("mac")) {
+					cmd = "pkill safaridriver";
+				}
+			}
+
+			if (driver != null) {
+				driver.manage().deleteAllCookies();
+				driver.quit();
+			}
+		} catch (Exception e) {
+			log.info(e.getMessage());
+		} finally {
+			try {
+				Process process = Runtime.getRuntime().exec(cmd);
+				process.waitFor();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
