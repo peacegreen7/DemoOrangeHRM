d3cc9015efca1b14f768d6a3ec8cb2e7b2c6a79a
snoopy <thanhyen107@gmail.com>
2021-10-24 15:17:44 +0700

    config log4j

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,13 @@
 package commons;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class BaseTest {
 
+	protected final Log log;
+
+	protected BaseTest() {
+		log = LogFactory.getLog(getClass());
+	}
 }
